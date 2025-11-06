41a11f7a3d2c528eee87895fd30e53d0c8a81d2e
catrionamurray <Catriona.Murray@colorado.edu>
2025-11-06 12:53:17 -0700

    wrote multiply function

--- src/math_utils.py
+++ src/math_utils.py
@@ -8,4 +8,12 @@ def divide(num1, num2):
     pass
 
 def multiply(num1, num2):
-    pass
+    # function to multiple two numbers together
+    # -----------------
+    # parameters:
+    #   num1: integer or float (hopefully!)
+    #   num2: integer or float (hopefully!)
+    #  returns:
+    #   num1*num2: integer or float (hopefully!)
+
+    return num1*num2
